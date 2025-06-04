fd5aabeb6f19cda69cea554694dc843f4964e578
Jaykumaran R <r1ajayakumaran1751994@gmail.com>
2025-06-04 11:45:34 +0530

    Docker GPG Conflict issue fix: https://github.com/ros/rosdistro/issues/46311

--- ros_tracking_package/ros2_ws/src/sim_cam_pkg/docker-entrypoint.sh
+++ ros_tracking_package/ros2_ws/src/sim_cam_pkg/docker-entrypoint.sh
@@ -6,21 +6,20 @@ if [ -f "/opt/ros/${ROS_DISTRO}/setup.bash" ]; then
     source "/opt/ros/${ROS_DISTRO}/setup.bash"
     echo "Sourced ROS 2 ${ROS_DISTRO} setup."
 else
-    echo "ROS 2 ${ROS_DISTRO} setup.bash not found."
+    echo "Error: ROS 2 ${ROS_DISTRO} setup.bash not found."
 fi 
 
-# Source the workspace's setup file
-WS_SETUP_FILE="/ros2_ws/install/setup.bash"
+# Source the workspace's setup file from /ros2_ws (which is the WORKDIR)
+WS_SETUP_FILE="/ros2_ws/install/setup.bash" 
 if [ -f "${WS_SETUP_FILE}" ]; then
    source "${WS_SETUP_FILE}"
    echo "Sourced custom workspace (${WS_SETUP_FILE})."
 else
-   echo "Custom workspace setup file (${WS_SETUP_FILE}) not found."
-fi # fi is just if spelled backward, just indicates the end of the if statement
+   echo "Error: Custom workspace setup file (${WS_SETUP_FILE}) not found."
+fi
 
 # Execute the command passed into 'docker run' (or the CMD in Dockerfile)
 exec "$@"
-
 
 # ** Important:  Before building the package, I've to do this to make the docker file executable**
 # chmod +x docker-entrypoint.sh
